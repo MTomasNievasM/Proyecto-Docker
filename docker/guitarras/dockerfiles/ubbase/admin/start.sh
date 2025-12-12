bd2af1f6c02794ca869fdb112feb596a84e6f9b7
MTomasNievasM <marcelotomasnm@gmail.com>
2025-12-12 18:46:40 +0100

    Deploy del contenedor

---
+++ docker/guitarras/dockerfiles/ubbase/admin/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+source /root/admin/base/usuario/mainuser.sh
+source /root/admin/base/ssh/mainssh.sh
+source /root/admin/base/sudo/mainsudo.sh
+
+main(){
+    # Gestión de usuario --> getuser.sh
+    # Gestión del sudo --> getsudo.sh
+    # ...
+    mkdir -p /root/logs
+    touch /root/logs/informe.log
+    newUser
+    resuser=$?
+
+        configurar_sudo
+        configurar_ssh
+
+
+    # Encargada de mantener el contenedor en ejecución de Background
+    #tail -f /dev/null   
+}
+
+main
