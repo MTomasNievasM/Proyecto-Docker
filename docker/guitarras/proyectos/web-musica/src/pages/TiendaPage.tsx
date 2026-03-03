827ae7f5633d35820e05b4ea1b4a30c5bbcc67b2
MTomasNievasM <marcelotomasnm@gmail.com>
2026-03-03 19:57:07 +0100

    Guitarras borrado

--- docker/guitarras/proyectos/web-musica/src/pages/TiendaPage.tsx
+++
@@ -1,9 +0,0 @@
-const TiendaPage = () => {
-  return (
-    <div>
-      <h2>Tienda</h2>
-      <p>Bienvenido a nuestra tienda. ¡Productos próximamente!</p>
-    </div>
-  )
-}
-export default TiendaPage
